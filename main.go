9a0a06c9501f31440fada4008e127f7ad03de5a7
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-06-15 21:17:02 -0600

    update database

--- main.go
+++ main.go
@@ -3,20 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
-<<<<<<< HEAD
 	"log"
 	"net/http"
+	"text/template"
 
 	firebase "firebase.google.com/go"
 	"google.golang.org/api/option"
-	"text/template"
 )
 
 // templates is a global variable that holds the parsed HTML templates.
 var templates = template.Must(template.ParseGlob("templates/*"))
 
-// main is the entry point of the application.
-func main() {
+// start is the handler function for the root URL ("/").
+func start(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 
 	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
@@ -29,84 +28,38 @@ func main() {
 		log.Fatalf("Failed to initialize Firebase app: %v", err)
 	}
 
-	// Registering the start function to handle requests to the root URL ("/").
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Firebase Realtime Database reference
-		dbClient, err := app.Database(ctx)
-		if err != nil {
-			log.Fatalf("Failed to get database client: %v", err)
-		}
-
-		// Example: Writing data to Firebase Realtime Database
-		ref := dbClient.NewRef("employees")
-		err = ref.Set(ctx, map[string]interface{}{
-			"name":  "Javier",
-			"email": "jepon26@gmail.com",
-		})
-		if err != nil {
-			log.Fatalf("Failed to write data: %v", err)
-		}
-
-		// Writing the "Hello World" message to the ResponseWriter, which sends it as the response to the client.
-		templates.ExecuteTemplate(w, "start", nil)
-	})
-
-	http.HandleFunc("/create", func(w http.ResponseWriter, r *http.Request) {
-		templates.ExecuteTemplate(w, "create", nil)
-	})
-=======
-	"text/template"
-	"net/http"
-	_ "github.com/go-sql-driver/mysql"
-)
-
-// DBconnection establishes a connection to the database.
-func DBconnection() *sql.DB {
-	Driver := "mysql"
-	User := ""
-	Password := ""
-	Name := "System"
-
-	connection, err := sql.Open(Driver, User+":"+Password+"@tcp(127.0.0.1)/"+Name)
+	// Firebase Realtime Database reference
+	dbClient, err := app.Database(ctx)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Failed to get database client: %v", err)
 	}
-	return connection
-}
 
-// templates is a global variable that holds the parsed HTML templates.
-var templates = template.Must(template.ParseGlob("templates/*"))
-
-// start is the handler function for the root URL ("/").
-func start(w http.ResponseWriter, r *http.Request) {
-	establishedConnection := DBconnection()
-
-	// Inserting a record into the "employees" table.
-	insertRecords, err := establishedConnection.Prepare("INSERT INTO employees(name, email) VALUES('Javier','jepon26@gmail.com')")
+	// Example: Writing data to Firebase Realtime Database
+	ref := dbClient.NewRef("employees")
+	err = ref.Set(ctx, map[string]interface{}{
+		"name":  "Javier",
+		"email": "jepon26@gmail.com",
+	})
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Failed to write data: %v", err)
 	}
-	insertRecords.Exec()
 
 	// Writing the "Hello World" message to the ResponseWriter, which sends it as the response to the client.
 	templates.ExecuteTemplate(w, "start", nil)
 }
 
-// Create is the handler function for the "/create" URL.
-func Create(w http.ResponseWriter, r *http.Request) {
+// create is the handler function for the "/create" URL.
+func create(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "create", nil)
 }
 
 // main is the entry point of the application.
 func main() {
-	// Registering the start function to handle requests to the root URL ("/").
+	// Register the handler functions
 	http.HandleFunc("/", start)
-	// Registering the Create function to handle requests to the "/create" URL.
-	http.HandleFunc("/create", Create)
->>>>>>> e2ef634e75f09c51c089da1fce00dce8282711b5
+	http.HandleFunc("/create", create)
 
-	fmt.Println("Server started....")
-
-	// Starting the HTTP server on port 8080 to listen for incoming requests.
-	http.ListenAndServe(":8080", nil)
+	// Start the HTTP server
+	fmt.Println("Server listening on http://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
